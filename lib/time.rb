af3ba1e85b7f775b1f7ee7b55271a4575cbeeb67
Demis Lavrentidis <demilavr@gmail.com>
2022-05-05 17:21:33 +0100

    Correction

--- lib/time.rb
+++ lib/time.rb
@@ -11,9 +11,8 @@ class ReadingTime
 		@convert =  @size.to_f * 60 / 200
 	end
 	
-	def time_required
+	def time_required()
 		fail "Enter text to be read" if @size.zero?
 		return "#{@convert} seconds"
-		
 	end
 end
--- spec/time_spec.rb
+++ spec/time_spec.rb
@@ -1,19 +1,18 @@
 require 'time'
 
 RSpec.describe ReadingTime do
-	it "counts the words inside the string and estimates the requred time for reading" do
+	it 'fails' do
 		rt = ReadingTime.new("")
 		rt.size("")
-		time = rt.time_required
-		expect  { time }.to raise_error "Enter text to be read"
+    expect  { rt.time_required() }.to raise_error "Enter text to be read"
 	end
 
 	context "return time if words are more than one" do
 		it "return time for the given string" do
 			rt = ReadingTime.new("ana, demis etc")
-			rt.size("ana, demis etc")
-			time = rt.time_required
-			expect (time).to eq "0.9 seconds"
+      rt.size("ana, demis etc")
+      time = rt.convert
+			expect(rt.time_required()).to eq "#{time} seconds"
 		end
 	end
 end
